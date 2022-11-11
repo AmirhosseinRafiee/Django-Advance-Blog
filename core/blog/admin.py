b4132e5d01a650e042bb598fd3159e0f5516b453
Amirhossein Rafiee <amirhossein.rafiee.80@gmail.com>
2022-11-12 02:00:09 +0330

    create blog app and accounts
    blog:
    + simple model for post and category
    
    accounts:
    + custom model for user
    + profile model for each user
    + signal for connecting each user to its profile when created

---
+++ core/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post, Category
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'status', 'categoty', 'created_date', 'published_date')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
--- core/core/settings.py
+++ core/core/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from email.policy import default
 from pathlib import Path
 from decouple import config
 
@@ -39,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager config
+AUTH_USER_MODEL = 'accounts.User'
