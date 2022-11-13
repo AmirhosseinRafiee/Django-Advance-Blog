e11c3c3973a4c2438b685c43e193ca05d25f6f0d
Amirhossein Rafiee <amirhossein.rafiee.80@gmail.com>
2022-11-13 19:50:27 +0330

    redirect view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from . import views
 
+app_name = "blog"
 
 urlpatterns = [
-    path('fbv-index', views.index_view, name='fbv-index'),
     # path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={'name': 'hasan'}), name='cbv-index'),
     path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
+    # path('go-to-geeksforgeeks', RedirectView.as_view(url="https://geeksforgeeks.org/"), name='redirect-to-geeksforgeeks'),
+    # path('go-to-cbv-index', RedirectView.as_view(pattern_name="blog:cbv-index"), name='redirect-to-cbv-index'),
+    path('go-to-geeksforgeeks', views.RedirectToGeeks.as_view(), name='redirect-to-geeksforgeeks'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
 
+''' Function based view show a template
 def index_view(request):
+    """
+    a function based view to show index page
+    """
     name = 'ali'
     context = {'name': name}
     return render(request, 'index.html', context)
-
+'''
 class IndexView(TemplateView):
+    '''
+    a class based view to show index page
+    '''
     template_name = 'index.html'
     
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['name'] = 'amir'
         context['posts'] = Post.objects.all()
         return context
+
+''' FBV for redirect
+from django.shortcuts import redirect
+def redirect_to_geeks(request):
+    return redirect('https://geeksforgeeks.org')
+'''
+
+class RedirectToGeeks(RedirectView):
+    '''
+    Redirection view sample for maktabkhooneh
+    '''
+    url = "https://geeksforgeeks.org"
+
+    def get_redirect_url(self, *args, **kwargs):
+        return super().get_redirect_url(*args, **kwargs)
