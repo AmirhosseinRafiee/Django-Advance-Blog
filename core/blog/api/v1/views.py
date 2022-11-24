f9ea441bb2d5fae9a6cc5aa1107d31894c19bf8b
Amirhossein Rafiee <amirhossein.rafiee.80@gmail.com>
2022-11-24 19:38:24 +0330

    adding permissions for two api

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
@@ -7,6 +8,7 @@ from ...models import Post
 
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def post_list(request):
     if request.method == "GET":
         posts = Post.objects.filter(status=True)
@@ -20,6 +22,7 @@ def post_list(request):
 
 
 @api_view(["GET", "PUT", "DELETE"])
+@permission_classes([IsAuthenticated])
 def post_detail(request, id):
     post = get_object_or_404(Post, pk=id, status=True)
     if request.method == "GET":
