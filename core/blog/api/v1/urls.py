9bf27672abaa144ee1796c70a058ffb77c962c51
Amirhossein Rafiee <amirhossein.rafiee.80@gmail.com>
2022-11-25 05:31:45 +0330

    adding list/create apiview

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('post/', views.post_list, name='post-list'),
+    # path('post/', views.post_list, name='post-list'),
+    path('post/', views.PostListView.as_view(), name='post-list'),
     path('post/<int:id>/', views.post_detail, name='post-detail'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,13 +1,13 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .serializers import PostSerializer
 from ...models import Post
 
-
-@api_view(["GET", "POST"])
+"""@api_view(["GET", "POST"])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def post_list(request):
     if request.method == "GET":
@@ -18,8 +18,23 @@ def post_list(request):
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data)"""
+
+class PostListView(APIView):
+    """get a list of posts and create new post"""
+
+    """return a list of posts"""
+    def get(self, request):
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
 
+    """create a post with provided data"""
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 @api_view(["GET", "PUT", "DELETE"])
 @permission_classes([IsAuthenticated])
