a2e28fbc6eba164e302bf772eb659fcd003228b2
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-03-19 07:37:51 +0330

    added tests for unittest

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -7,7 +7,7 @@ app_name = "blog"
 
 urlpatterns = [
     # path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={'name': 'hasan'}), name='cbv-index'),
-    path("cbv-index/", views.IndexView.as_view(), name="cbv-index"),
+    path("index/", views.IndexView.as_view(), name="index"),
     # path('go-to-geeksforgeeks', RedirectView.as_view(url="https://geeksforgeeks.org/"), name='redirect-to-geeksforgeeks'),
     # path('go-to-cbv-index', RedirectView.as_view(pattern_name="blog:cbv-index"), name='redirect-to-cbv-index'),
     path(
