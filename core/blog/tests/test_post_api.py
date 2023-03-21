746f594b12ccec17711c89a2229271e74b6cf73f
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-03-21 20:29:38 +0330

    just test merge with CI

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,4 +1,4 @@
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from datetime import datetime
