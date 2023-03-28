41a3911de5b23f0238cc67b725d0cf9affd87bfa
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-03-28 04:19:38 +0330

    simple example cache

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core.cache import cache
+import requests
 from .tasks import send_email_task
 
 
 def send_email(request):
     send_email_task.delay()
     return HttpResponse("<h1>Done Sending</h1>")
+
+def test_view(request):
+    if cache.get("test_delay_api") is None:
+        response = requests.get("https://07b35e6e-6cbd-41d5-9093-a84d0b388049.mock.pstmn.io/test/delay/5")
+        cache.set("test_delay_api", response.json(), 2)
+    return JsonResponse(cache.get("test_delay_api"))
