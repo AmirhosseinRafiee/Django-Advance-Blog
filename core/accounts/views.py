1fcae5692cbe8aeefd7eff6f62108e93117cc284
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-03-28 05:11:06 +0330

    update caching

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.cache import cache_page
 from django.core.cache import cache
 import requests
 from .tasks import send_email_task
@@ -8,8 +9,7 @@ def send_email(request):
     send_email_task.delay()
     return HttpResponse("<h1>Done Sending</h1>")
 
+@cache_page(60)
 def test_view(request):
-    if cache.get("test_delay_api") is None:
-        response = requests.get("https://07b35e6e-6cbd-41d5-9093-a84d0b388049.mock.pstmn.io/test/delay/5")
-        cache.set("test_delay_api", response.json(), 2)
-    return JsonResponse(cache.get("test_delay_api"))
+    response = requests.get("https://07b35e6e-6cbd-41d5-9093-a84d0b388049.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
