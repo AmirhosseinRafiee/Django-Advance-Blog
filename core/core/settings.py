76f91f274020ed479b7999820944cabd85badbc6
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-03-31 03:39:42 +0330

    connecting to gunicorn

--- core/core/settings.py
+++ core/core/settings.py
@@ -167,13 +167,13 @@ REST_FRAMEWORK = {
 }
 
 # email configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_USE_TLS = False
-EMAIL_HOST = "smtp-server"
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
-EMAIL_PORT = 1025
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_USE_TLS = False
+# EMAIL_HOST = "smtp-server"
+# EMAIL_HOST_USER = ""
+# EMAIL_HOST_PASSWORD = ""
+# EMAIL_PORT = 1025
 
 # simplejwt configuration
 SIMPLE_JWT = {
