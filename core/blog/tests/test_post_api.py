92b0cfa1ab22265c0da98cd5fbe4caf6e70c600e
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-04-15 03:23:43 +0330

    update api tests

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -6,19 +6,18 @@ import pytest
 from accounts.models import Profile
 from ..models import Post
 
-# @pytest.fixture
-# def client():
-#     client = APIClient()
-#     return client
 
+@pytest.fixture
+def api_client():
+    client = APIClient()
+    return client
 
 @pytest.fixture
 def common_user():
     user = get_user_model().objects.create_user(
-        email="test@test.com", password="a/@12345", is_verified=True
+        email="test@test.com", password="a/@123456", is_verified=True
     )
     return user
-
 
 @pytest.fixture
 def post_object(common_user):
@@ -32,7 +31,6 @@ def post_object(common_user):
     )
     return post
 
-
 @pytest.fixture
 def common_user_2():
     user = get_user_model().objects.create_user(
@@ -43,20 +41,19 @@ def common_user_2():
 
 @pytest.mark.django_db
 class TestPostApi:
-    client = APIClient()
 
-    def test_get_post_list_response_200_status(self):
+    def test_get_post_list_response_200_status(self, api_client):
         url = reverse("blog:api-v1:post-list")
-        response = self.client.get(url)
+        response = api_client.get(url)
         assert response.status_code == 200
 
-    def test_get_post_detail_response_200_status(self, post_object):
+    def test_get_post_detail_response_200_status(self, api_client, post_object):
         post = post_object
         url = reverse("blog:api-v1:post-detail", kwargs={"pk": post.id})
-        response = self.client.get(url)
+        response = api_client.get(url)
         assert response.status_code == 200
 
-    def test_create_post_response_401_status(self):
+    def test_create_post_response_401_status(self, api_client):
         url = reverse("blog:api-v1:post-list")
         data = {
             "title": "test title",
@@ -64,17 +61,17 @@ class TestPostApi:
             "status": True,
             "published_date": datetime.now(),
         }
-        response = self.client.post(url, data)
+        response = api_client.post(url, data)
         assert response.status_code == 401
 
-    def test_delete_post_response_401_status(self, post_object):
+    def test_delete_post_response_401_status(self, api_client, post_object):
         post = post_object
         url = reverse("blog:api-v1:post-detail", kwargs={"pk": post.id})
 
-        response = self.client.delete(url)
+        response = api_client.delete(url)
         assert response.status_code == 401
 
-    def test_edit_post_response_401_status(self, post_object):
+    def test_edit_post_response_401_status(self, api_client, post_object):
         post = post_object
         url = reverse("blog:api-v1:post-detail", kwargs={"pk": post.id})
         data = {
@@ -83,14 +80,14 @@ class TestPostApi:
             "status": True,
             "published_date": datetime.now(),
         }
-        response = self.client.put(url, data=data)
+        response = api_client.put(url, data=data)
         assert response.status_code == 401
         data2 = {"title": "edited title"}
-        response = self.client.patch(url, data=data2)
+        response = api_client.patch(url, data=data2)
         assert response.status_code == 401
 
     # user is authenticated from force_authenticate called
-    def test_create_post_response_201_status(self, common_user):
+    def test_create_post_response_201_status(self, api_client, common_user):
         url = reverse("blog:api-v1:post-list")
         data = {
             "title": "test title",
@@ -98,28 +95,33 @@ class TestPostApi:
             "status": True,
             "published_date": datetime.now(),
         }
-        self.client.force_authenticate(user=common_user)
-        response = self.client.post(url, data)
+        api_client.force_authenticate(user=common_user)
+        response = api_client.post(url, data)
         assert response.status_code == 201
         assert Post.objects.get(title="test title")
 
-    def test_create_post_invalid_data_response_400_status(self):
+    def test_create_post_invalid_data_response_400_status(self, api_client, common_user):
         url = reverse("blog:api-v1:post-list")
         data = {
             "title": "test title",
             "content": "test content",
         }
-        response = self.client.post(url, data)
+        api_client.force_authenticate(user=common_user)
+        response = api_client.post(url, data)
         assert response.status_code == 400
 
-    def test_delete_post_response_200_status(self, post_object):
+    def test_delete_post_response_204_status(self, api_client, common_user, post_object):
         post = post_object
         url = reverse("blog:api-v1:post-detail", kwargs={"pk": post.id})
-        response = self.client.delete(url)
+        # api_client.login(email="test@test.com", password="a/@123456")
+        api_client.force_authenticate(user=common_user)
+        print(api_client.__dict__["renderer_classes"]["json"])
+        print(api_client.__dict__["renderer_classes"]["multipart"])
+        response = api_client.delete(url)
         assert response.status_code == 204
         assert not Post.objects.filter(id=post.id).exists()
 
-    def test_edit_post_response_200_status(self, post_object):
+    def test_edit_post_response_200_status(self, api_client, common_user, post_object):
         post = post_object
         url = reverse("blog:api-v1:post-detail", kwargs={"pk": post.id})
         data = {
@@ -128,28 +130,31 @@ class TestPostApi:
             "status": True,
             "published_date": datetime.now(),
         }
-        response = self.client.put(url, data=data)
+        api_client.force_authenticate(user=common_user)
+        # api_client.login(email="test@test.com", password="a/@123456")
+        response = api_client.put(url, data=data)
         assert response.status_code == 200
         post.refresh_from_db()
         assert post.title == "new test title"
         data2 = {"title": "edited title"}
-        response = self.client.patch(url, data=data2)
+        response = api_client.patch(url, data=data2)
         assert response.status_code == 200
         post.refresh_from_db()
         assert post.title == "edited title"
 
     # user2 is authenticated from force_authenticate called
-    def test_delete_post_response_403_status(self, post_object, common_user_2):
+    def test_delete_post_response_403_status(self, api_client, common_user_2, post_object):
         user2 = common_user_2
-        self.client.logout()
-        self.client.force_authenticate(user=user2)
+        api_client.logout()
+        api_client.force_authenticate(user=user2)
         post = post_object
         url = reverse("blog:api-v1:post-detail", kwargs={"pk": post.id})
-        response = self.client.delete(url)
+        response = api_client.delete(url)
         assert response.status_code == 403
         assert Post.objects.get(id=post.id)
 
-    def test_edit_post_response_403_status(self, post_object):
+    def test_edit_post_response_403_status(self, api_client, common_user_2, post_object):
+        user2 = common_user_2
         post = post_object
         url = reverse("blog:api-v1:post-detail", kwargs={"pk": post.id})
         data = {
@@ -158,8 +163,10 @@ class TestPostApi:
             "status": True,
             "published_date": datetime.now(),
         }
-        response = self.client.put(url, data=data)
+        # api_client.login(email="test2@test.com", password="a/@123456")
+        api_client.force_authenticate(user=user2)
+        response = api_client.put(url, data=data)
         assert response.status_code == 403
         data2 = {"title": "edited title"}
-        response = self.client.patch(url, data=data2)
+        response = api_client.patch(url, data=data2)
         assert response.status_code == 403
